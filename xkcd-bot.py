2c9778e8dc86d2c1b1b9716b39efddd44fbccbdb
AlicornIndustries <ormagge@gmail.com>
2017-07-06 12:07:16 -0700

    Add files via upload

--- xkcd-bot.py
+++ xkcd-bot.py
@@ -1,29 +1,37 @@
-## XKCD-bot v1.0
+## xkcd-bot v2.0
 ## Author: Soma // Alicorn Industries
-## !xkcd to summon current XKCD
+## !xkcd to summon current xkcd
 
-####### DON'T FORGET TO PUT YOUR CLIENT ID IN YOUR_CLIENT_ID_HERE
+## TODO: Add specific xkcd functionality
 
 import discord
-import asyncio
 import urllib.request, json
+from discord.ext.commands import Bot
 
-client = discord.Client()
+# bot is a subclass of client
+bot = Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith("!xkcd"):
-        # Get current XKCD
-        with urllib.request.urlopen('https://xkcd.com/info.0.json') as url:
-            data = json.loads(url.read().decode())
-            img_url = data['img']
-        await client.send_message(message.channel, img_url)
-        
-client.run("YOUR_CLIENT_ID_HERE")
+@bot.event
+async def on_read():
+    print('Client logged in')
+
+@bot.command()
+async def hello(*args):
+    return await bot.say('Hello, world!')
+
+@bot.command()
+async def xkcd(*args):
+    with urllib.request.urlopen('https://xkcd.com/info.0.json') as url:
+        data = json.loads(url.read().decode())
+        img_url = data['img']
+    await bot.say(img_url)    
+
+# Token here
+bot.run('MzMyNTY4Nzc4ODgzNzI3Mzkw.DEAaQQ.gmM-Xs3MonYBjh7T-OG7iatw5Z8')
